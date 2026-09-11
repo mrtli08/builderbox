a474aea11bc6f242dcbe66bcd0e80ed304a790ff
mrtli08 <cicekmertali08@gmail.com>
2026-09-11 14:23:50 +0300

    Update server.js
    Replaced 'disconnect' event with 'removePlayer' for better clarity.

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve static files from the current directory
 app.use(express.static(path.join(__dirname)));
 
 const players = {};
@@ -15,12 +14,11 @@ const players = {};
 io.on('connection', (socket) => {
     console.log(`Player connected: ${socket.id}`);
 
-    // Initialize player data
     players[socket.id] = {
         x: 0,
         y: 1,
         z: 0,
-        color: '#' + Math.floor(Math.random()*16777215).toString(16) // Random color
+        color: '#' + Math.floor(Math.random()*16777215).toString(16)
     };
 
     // Send existing players to the new player
@@ -29,25 +27,21 @@ io.on('connection', (socket) => {
     // Broadcast new player to everyone else
     socket.broadcast.emit('newPlayer', { id: socket.id, player: players[socket.id] });
 
-    // Handle player movement updates
-    socket.emit('currentPlayers', players);
-
     socket.on('playerMovement', (movementData) => {
         if (players[socket.id]) {
             players[socket.id].x = movementData.x;
             players[socket.id].y = movementData.y;
             players[socket.id].z = movementData.z;
             
-            // Broadcast movement to other players
             socket.broadcast.emit('playerMoved', { id: socket.id, x: movementData.x, y: movementData.y, z: movementData.z });
         }
     });
 
-    // Handle disconnection
     socket.on('disconnect', () => {
         console.log(`Player disconnected: ${socket.id}`);
         delete players[socket.id];
-        io.emit('disconnect', socket.id);
+        // Use a safe custom event name instead of 'disconnect'
+        io.emit('removePlayer', socket.id);
     });
 });
 
